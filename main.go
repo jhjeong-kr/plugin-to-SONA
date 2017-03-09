f14330315322967b3662168662fb1f50584cc2c7
Jin-Hwan Jeong <jhjeong.kr@gmail.com>
2017-03-08 16:44:48 -0800

    init

--- main.go
+++ main.go
@@ -1,50 +1,12 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"time"
-
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/pkg/api/v1"
-	"k8s.io/client-go/pkg/fields"
-	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/tools/clientcmd"
+	"os"
+	"plugin-to-SONA"
+	"plugin-to-SONA/config"
 )
 
 func main() {
-	kubeconfig := flag.String("kubeconfig", "/etc/kubernetes/admin.conf", "absolute path")
-	flag.Parse()
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	fmt.Println("Ok, let's enumerate")
-
-	watchlist := cache.NewListWatchFromClient(clientset.Core().RESTClient(), "pods", v1.NamespaceDefault,
-		fields.Everything())
-	_, controller := cache.NewInformer(
-		watchlist,
-		&v1.Pod{},
-		time.Second*0,
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				fmt.Printf("add: %s \n", obj)
-			},
-			DeleteFunc: func(obj interface{}) {
-				fmt.Printf("delete: %s \n", obj)
-			},
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				fmt.Printf("old: %s, new: %s \n", oldObj, newObj)
-			},
-		},
-	)
-	stop := make(chan struct{})
-	controller.Run(stop)
+	config.ParseCommandLine()
+	os.Exit(plugin.Run())
 }
